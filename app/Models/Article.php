1d406dd4712afd65656092fc479a2535e84cf29f
Erick EMiliani <martinoli944@gmail.com>
2021-11-26 15:01:31 -0600

    ADD: all models and fillable property

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
+
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'img',
+        'subtitle',
+        'body',
+        'category_id',
+        'img_id',
+    ];
 }
