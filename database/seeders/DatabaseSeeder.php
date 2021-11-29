bcbec5e0b0de453324fcc90ed019baf5e88aa6b0
Erick EMiliani <martinoli944@gmail.com>
2021-11-28 21:14:34 -0600

    ADD: layout dashboard template

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,12 +1,7 @@
 <?php
-
 namespace Database\Seeders;
 
-use App\Models\Article;
-use App\Models\Category;
-use App\Models\Images;
-use App\Models\Tags;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,11 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
-        Category::factory(5)->create();
-        Images::factory(5)->create();
-        Tags::factory(5)->create();
-        Article::factory(5)->create();
-
+        $this->call([UsersTableSeeder::class]);
     }
 }
